de1357ebe93f0fea684b9418112d8acc34226592
Daniel Villamil <dan.fv@outlook.com>
2021-09-18 15:03:17 -0700

    initial EDA for flights db

--- Queries.sql
+++ Queries.sql
@@ -6,4 +6,6 @@
 
 -- select * from flights order by random() limit 1000;
 
-select * from flights order by random() limit 50000;
+-- select * from flights order by random() limit 50000;
+
+select * from flights_test order by random() limit 50000;
